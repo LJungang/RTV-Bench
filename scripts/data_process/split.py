aa595b76a825f2ed1f5b80bc27f3c72eda365786
Jungang Li <jungang_li@foxmail.com>
2025-06-27 23:40:26 +0800

    Create split.py

---
+++ scripts/data_process/split.py
@@ -0,0 +1,36 @@
+import json
+import math
+import os
+import argparse
+from pathlib import Path
+
+def split_annotation(anno_path, args):
+    num_splits = args.num_splits
+    with open(anno_path) as f:
+        data = json.load(f)
+    
+    total = len(data)
+    per_split = math.ceil(total / num_splits)
+    
+    output_dir = Path(anno_path).parent / "splits"
+    output_dir.mkdir(exist_ok=True)
+    
+    for i in range(num_splits):
+        start = i * per_split
+        end = min((i+1)*per_split, total)
+        split_data = data[start:end]
+        
+        split_path = output_dir / f"split_{i+1}.json"
+        with open(split_path, 'w') as f:
+            json.dump(split_data, f, indent=2)
+            
+    print(f"generate {num_splits} subsets to {output_dir}.")
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--num_splits',required=True,default=4,type=int)
+    args = parser.parse_args()
+    if args.num_splits==4:
+        pass
+    else:
+        split_annotation("./data/RTV-Bench/annotation_constructed_1.json",args)
